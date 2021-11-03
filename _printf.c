6a19bdfc384ef538d804b64731177f65f436fd3a
MontyWilliams <3650@holbertonschool.com>
2021-11-03 14:17:05 -0600

    trying to set up branch

--- _printf.c
+++ _printf.c
@@ -1,23 +1,14 @@
 #include "main.h"
 #include <stdarg.h>
 #include <stdio.h>
+
 int _printf(const char *format, ...)
 {
-	char *_mv;
-	char *s;
-	unsigned int i;
+	int* argp = (int*)&format;
+	argp =+ sizeof(format) / sizeof(int);
+	argp++;
+	argp++;
+	argp++;
 
-	va_list arg;
-	va_start(arg, format);
 
-	*mv = format;
-
-	for (i = 0; *_mv[i] != '\0'; i++)
-	{
-		while (*mv !='\0')
-		{
-			if (*mv[i] == '%')
-				return (mv[i])
-		}
-	}
 }
