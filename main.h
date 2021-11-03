7ee9dc8aa89225f52c6e133585b5eed1f42d67bc
MontyWilliams <3650@holbertonschool.com>
2021-11-03 15:23:54 -0600

    add struct to header

--- main.h
+++ main.h
@@ -5,6 +5,18 @@
 #include <stdarg.h>
 #include <stdlib.h>
 
+/**
+ * struct print - structure for printing various types
+ * @spec: to print
+ * @f: print function
+ */
+
+typedef struct print
+{
+char *spec;
+int (*f)(va_list);
+} specifier;
+
 int _printf(const char *format, ...);
 int _putchar(char c);
 #endif
