a7ab846a424eb765be3d1f769cd00e921a2e3b58
MontyWilliams <3650@holbertonschool.com>
2021-11-04 08:17:49 -0600

    got the regular print portion to work

--- _printf.c
+++ _printf.c
@@ -2,8 +2,32 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+/**
+ * _printf - Prints out text and data using a format specifier like the
+ * standard printf()
+ * @format: The string part that includes the format
+ * Return: number of characters written
+ */
+
 int _printf(const char *format, ...)
 {
-	unsigned int i;
-	
+	unsigned int i = 0;
+	int printedchars = 0;
+	va_list ap;
+
+	va_start(ap, format);
+
+	while (format[i] != '\0')
+	{
+		do {
+			_putchar(format[i]);
+			i++;
+		} while (format[i] == '%');
+
+	}
+	printedchars++;
+i++;
+
+va_end(ap);
+return (printedchars);
 }
