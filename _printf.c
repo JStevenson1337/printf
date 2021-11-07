6805b485f17f652466bcf1075d9a199f5f0ddd97
Jeremy Stevenson <jeremy.m.stevenson@gmail.com>
2021-11-06 21:30:13 -0500

    pushing for checker

--- _printf.c
+++ _printf.c
@@ -1,10 +1,6 @@
 #include "main.h"
 #include <stdarg.h>
-<<<<<<< HEAD
-
-=======
 #include <stdlib.h>
->>>>>>> M2
 
 /**
  * _printf - prints strings ints chars
@@ -13,53 +9,12 @@
  */
 int _printf(const char *format, ...)
 {
-<<<<<<< HEAD
-	char modify_t;
-
-	unsigned int i = 0;
-	int printedchars = 0;
-	va_list args;
-	char *p_fmt, *s;
-
-	va_start(args, format);
-
-=======
 	va_list args;
 	unsigned int i = 0, printedchar = 0;
->>>>>>> M2
 
 	va_start(args, format);
 	while (format[i] != '\0')
 	{
-<<<<<<< HEAD
-		if (format[i] == '%')
-		{
-
-			i++;
-			int (* f_func(char spec ))(va_list *args);
-			{
-				print_t pa[] = {
-					{'c', print_char},
-					// {'s', print_string},
-					// {'d', print_int},
-					// {'i', print_int},
-					//{'b', print_binary},
-					//{'o', print_octal},
-					//{'u', print_unsigned},
-					//{'x', print_hex},
-					//{'X', print_HEX},
-					//{'%', print_percent},
-					//{NULL, NULL}
-				};
-			}
-		}
-	printedchars++;
-i++;
-	}
-va_end(args);
-return (printedchars);
-}
-=======
 		if (!format)
 		return (-1);
 		else if (format[i] == '%')
@@ -90,4 +45,3 @@ return (printedchars);
 		i++; }
 	va_end(args);
 	return (printedchar); }
->>>>>>> M2
--- main.h
+++ main.h
@@ -7,35 +7,11 @@
 #include <stdlib.h>
 #include <limits.h>
 
-<<<<<<< HEAD
-
-/**
- * struct print - structure for printing various types
- * @format: to print
- * @f: print function
- */
-
-typedef struct print_t
-{
-	char p_fmt;
-	int (*f)(va_list *);
-} print_t;
-=======
->>>>>>> M2
-
-int print_char(va_list *args);
 int _printf(const char *format, ...);
 int _putchar(char c);
-<<<<<<< HEAD
-int _strlen(char *s);
-char *_strcpy(char *dest, char *src);
-char* _iota(int num, char *str);
-int (* f_func(char spec))(va_list *args);
-=======
 int percent(void);
 int print_string(char *sp);
 int print_char(char c);
 int print_int(int n);
->>>>>>> M2
 
 #endif
