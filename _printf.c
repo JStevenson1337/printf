3d131863b70a90d117fa8179b9b95a8274d22bb8
Jeremy Stevenson <jeremy.m.stevenson@gmail.com>
2021-11-06 20:07:13 -0500

    Working Branch

--- _printf.c
+++ _printf.c
@@ -1,11 +1,5 @@
 #include "main.h"
 #include <stdarg.h>
-<<<<<<< HEAD
-
-=======
-#include <stdlib.h>
->>>>>>> M2
-
 /**
  * _printf - prints strings ints chars
  * @format: format
@@ -13,54 +7,13 @@
  */
 int _printf(const char *format, ...)
 {
-<<<<<<< HEAD
-	char modify_t;
-
-	unsigned int i = 0;
-	int printedchars = 0;
-	va_list args;
-	char *p_fmt, *s;
-
-	va_start(args, format);
-
-=======
 	va_list args;
 	unsigned int i = 0, printedchar = 0;
->>>>>>> M2
 
-	va_start(args, format);
+va_start(args, format);
 	while (format[i] != '\0')
 	{
-<<<<<<< HEAD
-		if (format[i] == '%')
-		{
-
-			i++;
-			int (* f_func(char spec ))(va_list *args);
-			{
-				print_t pa[] = {
-					{'c', print_char},
-					// {'s', print_string},
-					// {'d', print_int},
-					// {'i', print_int},
-					//{'b', print_binary},
-					//{'o', print_octal},
-					//{'u', print_unsigned},
-					//{'x', print_hex},
-					//{'X', print_HEX},
-					//{'%', print_percent},
-					//{NULL, NULL}
-				};
-			}
-		}
-	printedchars++;
-i++;
-	}
-va_end(args);
-return (printedchars);
-}
-=======
-		if (!format)
+	if (!format)
 		return (-1);
 		else if (format[i] == '%')
 		{
@@ -90,4 +43,4 @@ return (printedchars);
 		i++; }
 	va_end(args);
 	return (printedchar); }
->>>>>>> M2
+
